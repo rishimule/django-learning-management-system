7f82ba72fffbf57276ede77a33feb77d8f5d4e33
Rishi Mule <radonintro1234@gmail.com>
2022-04-29 11:08:25 +0530

    security key

--- mylms/custom_azure.py
+++ mylms/custom_azure.py
@@ -1,15 +1,16 @@
 from storages.backends.azure_storage import AzureStorage
+from django.conf import settings
 
 class AzureMediaStorage(AzureStorage):
-    account_name = 'lmsdjangoaccountstorage' # Must be replaced by your <storage_account_name>
-    account_key = 'K0xDIB/z2pVupQT/83rStMOcl+mR7kaYIbQQyODHl+FfpjvhBsHQfYvFaFslLoKb22a6K681Bg3LRgkzQrMdng==' # Must be replaced by your <storage_account_key>
+    account_name = settings.AZURE_ACCOUNT_NAME # Must be replaced by your <storage_account_name>
+    account_key = settings.AZURE_STORAGE_ACCOUNT_KEY # Must be replaced by your <storage_account_key>
     azure_container = 'media'
     expiration_secs = None
     # file_overwrite = False
 
 class AzureStaticStorage(AzureStorage):
-    account_name = 'lmsdjangoaccountstorage' # Must be replaced by your storage_account_name
-    account_key = 'K0xDIB/z2pVupQT/83rStMOcl+mR7kaYIbQQyODHl+FfpjvhBsHQfYvFaFslLoKb22a6K681Bg3LRgkzQrMdng==' # Must be replaced by your <storage_account_key>
+    account_name = settings.AZURE_ACCOUNT_NAME # Must be replaced by your storage_account_name
+    account_key = settings.AZURE_STORAGE_ACCOUNT_KEY # Must be replaced by your <storage_account_key>
     azure_container = 'static'
     expiration_secs = None
     # file_overwrite = False
--- mylms/settings.py
+++ mylms/settings.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 from pathlib import Path
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%tn=cmyc4r(y&&h*s^im0*2ymp3wls=+%galyup3mx1vt&kzpo'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -148,7 +152,9 @@ STATICFILES_STORAGE = 'mylms.custom_azure.AzureStaticStorage'
 STATIC_LOCATION = "static"
 MEDIA_LOCATION = "media"
 
-AZURE_ACCOUNT_NAME = "lmsdjangoaccountstorage"
+AZURE_ACCOUNT_NAME = env('AZURE_ACCOUNT_NAME')
+AZURE_STORAGE_ACCOUNT_KEY = env('AZURE_STORAGE_ACCOUNT_KEY')
+
 AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
 STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
 MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
